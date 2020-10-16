0d89c52f32d1ccf47f34ecb1cd22612b049e72e5
Hadas Orgad <t-haorga@microsoft.com>
2020-10-16 22:53:00 +0300

    changed checkpoint line

--- train_script.py
+++ train_script.py
@@ -15,7 +15,7 @@ enc = enc.to(device)
 
 print(f"model length: {len(list(enc.parameters()))}")
 
-# enc.load_state_dict(torch.load('checkpoints/linear_without_relu_ckpt_4800.pth')['model'])
+enc.load_state_dict(torch.load(f'checkpoints/{run_name}_ckpt_1650.pth')['model'])
 
 from Train_sta import train
 from Utils import SignalsDataset
@@ -23,7 +23,7 @@ from torch.utils.data import DataLoader
 from torch.optim import SGD
 from torch.optim import Adam
 
-ds_train = SignalsDataset('train_data_signal__EsinX^2__amp_2__freq_2__noise_uniform__low_-2__high_2.npz')
+ds_train = SignalsDataset('train_data_signal__EsinX^2__amp_2__freq_2__noise_uniform__low_-1__high_1.npz')
 dl_train = DataLoader(ds_train, batch_size=batch_size, shuffle=True, drop_last=True)
 ds_val = SignalsDataset('val_data_signal__EsinX^2__amp_2__freq_2__noise_uniform__low_-2__high_2.npz')
 dl_val = DataLoader(ds_val, batch_size=batch_size, shuffle=True, drop_last=True)
